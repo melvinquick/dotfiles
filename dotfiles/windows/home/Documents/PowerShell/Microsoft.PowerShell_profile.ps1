cebd65cc7753606e4576c0b84519855200440079
Melvin Quick <melvinquick@proton.me>
2025-02-19 14:21:40 -0500

    Removed old data I no longer use. Renamed Refresh function to better_clear and set an alias of bclear for this function.

--- dotfiles/windows/home/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ dotfiles/windows/home/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,13 +1,4 @@
-# * IMPORTS
-
-Install-Module -Name "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1" -ErrorAction SilentlyContinue
-
 # * FUNCTIONS
-
-function Cleanup {
-  Invoke-WebRequest wch.melvinquick.com | Invoke-Expression 
-}
-
 function Clear-CommandHistory {
   $null > (Get-PSReadlineOption).HistorySavePath
 }
@@ -43,82 +34,14 @@ function Get-CurrentUsers {
   Write-Output $user
 }
 
-function Get-Latency {
-  param (
-    # * Time you'd like the function to stop running in 24 hour time (good values are 1, 2, 3, ..., 24) (default is 24)
-    [Parameter (Mandatory = $false)]
-    [ValidateSet (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24)]
-    [int]
-    $endTime = 24,
-
-    # * Computer Name, IP Address, or URL you'd like to ping (default is localhost)
-    [Parameter (Mandatory = $false)]
-    [string]
-    $pingAddress = "localhost",
-
-    # * The location you'd like the output to be saved (default is ~\Documents)
-    [Parameter (Mandatory = $false)]
-    [string]
-    $outputLocation = "~\Documents",
-
-    # * The name and type you'd like the output file to be saved as
-    [Parameter (Mandatory = $false)]
-    [string]
-    $outputName = "latency.csv",
-
-    # * The amount of time you'd like to wait between pings
-    [Parameter (Mandatory = $false)]
-    [int]
-    $waitTime = 10
-  )
-
-  # * Variables
-  $data = New-Object -TypeName psobject
-  $dataCollection = @()
-  $date = ""
-  $latency = 0
-  $output = "$outputLocation\$outputName"
-  $time = (Get-Date).Hour
-
-  # * Main
-  while ($time -lt $endTime) {
-
-    # * Get the date and then add it to $data
-    $date = Get-Date
-    $data | Add-Member -MemberType NoteProperty -Name Date -Value $date
-
-    # * Ping entered computer/ip/url and select only the response time then add it to $data
-    $latency = Test-Connection -Ping $pingAddress -Count 1 | ForEach-Object { $_.Latency }
-    $data | Add-Member -MemberType NoteProperty -Name Latency -Value $latency
-      
-    # * Put $data in $dataCollection
-    $dataCollection += $data
-
-    # * Export to CSV
-    $dataCollection | Export-Csv -LiteralPath $output
-
-    # * Reset $data back to a new PSObject
-    $data = New-Object -TypeName psobject
-
-    # * Sleep for 10 seconds
-    Start-Sleep -Seconds $waitTime
-      
-    # * Set time variable to check for end condition
-    $time = (Get-Date).Hour
-
-  }
-}
-
-function Refresh {
+function better_clear {
   & $PROFILE
 }
 
 # * ALIASES
-
-
+Set-Alias -Name bclear -Value better_clear
 
 # * STARTUP
-
 Invoke-Expression (&starship init powershell)
 Set-Location $HOME
 Clear-Host

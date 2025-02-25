1cb14489fb9d2c4d1c105bee5b9546973907b5ee
Melvin Quick <melvinquick@proton.me>
2025-02-25 11:28:29 -0500

    Reorganized config since not everything needs to be interactive specific.

--- dotfiles/linux/home/.config/fish/config.fish
+++ dotfiles/linux/home/.config/fish/config.fish
@@ -1,10 +1,16 @@
+# * Settings for only if shell is launched interactively
 if status is-interactive
-    # * Disable the Fish Interactive Shell Greeting Message
-    set fish_greeting
 
-    # * Fastfetch Call
-    fastfetch --config ~/.config/fastfetch/fastfetch.jsonc
-
-    # * Starship Eval
-    starship init fish | source
 end
+
+# * Disable the Fish Interactive Shell Greeting Message
+set fish_greeting
+
+# * Fastfetch Call
+fastfetch --config ~/.config/fastfetch/fastfetch.jsonc
+
+# * FZF Better Support
+fzf --fish | source
+
+# * Starship Eval
+starship init fish | source

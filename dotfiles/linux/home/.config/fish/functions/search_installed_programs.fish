d2e506b557031bfe8a1a7f735f67600cc9379b53
Melvin Quick <melvinquick@proton.me>
2025-03-12 14:32:40 -0400

    Added logic so you don't have to pass a program name to these functions.

--- dotfiles/linux/home/.config/fish/functions/search_installed_programs.fish
+++ dotfiles/linux/home/.config/fish/functions/search_installed_programs.fish
@@ -1,3 +1,7 @@
 function search_installed_programs -a program_keyword -d "Pass a keyword to find any programs containing that keyword installed on your system"
-    nala search $program_keyword -i | grep $program_keyword | awk '{print $1}'
+    if test -z "$program_keyword"
+        nala list -i | grep 'is installed$' -B 1 | grep -v 'is installed$' | sed 's/^[ \t]*//' | sed 's/ .*$//' | grep -v '^--$'
+    else
+        nala search "$program_keyword" -i | grep "$program_keyword" | awk '{print $1}'
+    end
 end
--- dotfiles/linux/home/.config/fish/functions/search_repo_programs.fish
+++ dotfiles/linux/home/.config/fish/functions/search_repo_programs.fish
@@ -1,3 +1,7 @@
 function search_repo_programs -a program_keyword -d "Pass a keyword to find any programs containing that keyword in the repositories"
-    nala search $program_keyword | grep $program_keyword | awk '{print $1}'
+    if test -z "$program_keyword"
+        nala search | awk '{print $1}'
+    else
+        nala search $program_keyword | grep $program_keyword | awk '{print $1}'
+    end
 end

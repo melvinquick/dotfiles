5624cd1c7bde708b5b9dd1c26101d539696a5450
Melvin Quick <melvinquick@proton.me>
2025-03-12 15:21:03 -0400

    Corrected these to only allow you to pass a keyword as it didn't really work with no keyword getting passed.

--- dotfiles/linux/home/.config/fish/functions/search_installed_programs.fish
+++ dotfiles/linux/home/.config/fish/functions/search_installed_programs.fish
@@ -1,7 +1,7 @@
 function search_installed_programs -a program_keyword -d "Pass a keyword to find any programs containing that keyword installed on your system"
     if test -z "$program_keyword"
-        nala list -i | grep 'is installed$' -B 1 | grep -v 'is installed$' | sed 's/^[ \t]*//' | sed 's/ .*$//' | grep -v '^--$'
+        echo "You need to provide a keyword to search for! e.g., search_installed_programs kubuntu"
     else
-        nala search "$program_keyword" -i | grep "$program_keyword" | awk '{print $1}'
+        nala search $program_keyword -i | grep 'is installed$' -B 1 | grep -v 'is installed$' | sed 's/^[ \t]*//' | sed 's/ .*$//' | grep -v '^--$' | grep "$program_keyword" | awk '{print $1}'
     end
 end
--- dotfiles/linux/home/.config/fish/functions/search_repo_programs.fish
+++ dotfiles/linux/home/.config/fish/functions/search_repo_programs.fish
@@ -1,7 +1,7 @@
 function search_repo_programs -a program_keyword -d "Pass a keyword to find any programs containing that keyword in the repositories"
     if test -z "$program_keyword"
-        nala search | awk '{print $1}'
+        echo "You need to provide a keyword to search for! e.g., search_repo_programs kubuntu"
     else
-        nala search $program_keyword | grep $program_keyword | awk '{print $1}'
+        nala search $program_keyword | sed 's/^[ \t]*//' | sed 's/ .*$//' | grep -v '^--$' | grep $program_keyword | awk '{print $1}'
     end
 end

54fc10ed8d010b668ff5342537713924658d638b
Melvin Quick <melvinquick@proton.me>
2025-03-17 14:45:11 -0400

    Added aliases for functions.

--- dotfiles/linux/home/.config/fish/config.fish
+++ dotfiles/linux/home/.config/fish/config.fish
@@ -3,6 +3,16 @@ if status is-interactive
 
 end
 
+# * Set aliases for other commands and/or functions
+alias sip search_installed_programs
+alias srp search_repo_programs
+alias clear better_clear
+alias fzb fuzzy_bat
+alias pyver python_version
+alias upgrade upgrade_system
+alias osupgrade upgrade_release
+alias weather "weather_checker Clearfield Pennsylvania"
+
 # * Disable the Fish Interactive Shell Greeting Message
 set fish_greeting
 

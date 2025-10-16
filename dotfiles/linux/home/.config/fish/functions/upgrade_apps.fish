721fd383d3cfc1596109ca9c99ff3aa7079881b8
melvinquick <melvinquick@proton.me>
2025-10-16 11:10:11 -0400

    Corrected the package manager being used in the function description

--- dotfiles/linux/home/.config/fish/functions/upgrade_apps.fish
+++ dotfiles/linux/home/.config/fish/functions/upgrade_apps.fish
@@ -1,3 +1,3 @@
-function upgrade_apps -d "Use nala to upgrade system apps and autoremove apps / fix broken apps"
+function upgrade_apps -d "Use pacman to upgrade system apps and autoremove apps / fix broken apps"
     sudo pacman -Syu --noconfirm && sudo pacman -Sc --noconfirm
 end

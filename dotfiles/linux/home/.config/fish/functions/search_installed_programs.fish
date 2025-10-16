bc31b29e3db0e10f4962ea340ca9a2f3f6ba34a6
melvinquick <melvinquick@proton.me>
2025-10-16 12:04:36 -0400

    Modified these functions to use yay instead of pacman so I can search the aur as well.

--- dotfiles/linux/home/.config/fish/functions/search_installed_programs.fish
+++ dotfiles/linux/home/.config/fish/functions/search_installed_programs.fish
@@ -1,7 +1,12 @@
 function search_installed_programs -a program_keyword -d "Pass a keyword to find any programs containing that keyword installed on your system"
     if test -z "$program_keyword"
-        echo "You need to provide a keyword to search for! e.g., search_installed_programs kubuntu"
+        echo "You need to provide a keyword to search for! e.g., search_installed_programs fish"
     else
-        pacman -Qs $program_keyword | grep -e core/ -e extra/ -e community/ -e multilib/ -e testing/ -e staging/ -e aur/ -e local/
+        yay -Qs $program_keyword | grep -e core/ -e extra/ -e community/ -e multilib/ -e testing/ -e staging/ -e aur/ -e local/
+
+        set found_programs (yay -Qs $program_keyword)
+        if test -z "$found_programs"
+            echo "No programs found on the system containing the keyword '$program_keyword'"
+        end
     end
 end
--- dotfiles/linux/home/.config/fish/functions/search_repo_programs.fish
+++ dotfiles/linux/home/.config/fish/functions/search_repo_programs.fish
@@ -1,7 +1,12 @@
 function search_repo_programs -a program_keyword -d "Pass a keyword to find any programs containing that keyword in the repositories"
     if test -z "$program_keyword"
-        echo "You need to provide a keyword to search for! e.g., search_repo_programs kubuntu"
+        echo "You need to provide a keyword to search for! e.g., search_repo_programs fish"
     else
-        pacman -Ss $program_keyword | grep -e core/ -e extra/ -e community/ -e multilib/ -e testing/ -e staging/ -e aur/ -e local/
+        yay -Ss $program_keyword | grep -e core/ -e extra/ -e community/ -e multilib/ -e testing/ -e staging/ -e aur/ -e local/
+
+        set found_programs (yay -Ss $program_keyword)
+        if test -z "$found_programs"
+            echo "No programs found in the Standard Repositories, Chaotic AUR, or AUR containing the keyword '$program_keyword'"
+        end
     end
 end

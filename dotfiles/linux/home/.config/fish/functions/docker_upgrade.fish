e315d6db13128fdc4762a67700a506b153ecdb04
melvinquick <melvinquick@proton.me>
2026-03-12 12:11:06 -0400

    Updated docker_upgrade function so that it produces expected output on container upgrades now.

--- dotfiles/linux/home/.config/fish/functions/docker_upgrade.fish
+++ dotfiles/linux/home/.config/fish/functions/docker_upgrade.fish
@@ -1,15 +1,15 @@
 function docker_upgrade -d "While in a directory with a Docker Compose YAML file, running this function will run a pull and restart the container if a new image was found"
-    set old_image_count (docker image ls --format table | wc -l)
+    set old_images (docker compose images -q)
 
     docker compose pull >/dev/null 2>&1 && docker compose up -d >/dev/null 2>&1
 
-    set new_image_count (docker image ls --format table | wc -l)
+    docker image prune -af >/dev/null 2>&1
 
-    if test $old_image_count -ne $new_image_count
+    set new_images (docker compose images -q)
+
+    if test $old_images != $new_images
         echo "This container has been updated!"
     else
         echo "This container is already up to date!"
     end
-
-    docker image prune -af >/dev/null 2>&1
 end

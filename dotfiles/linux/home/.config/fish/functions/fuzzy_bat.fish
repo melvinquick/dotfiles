12b4d6791f7222a6c594c79baf315834104f679c
melvinquick <melvinquick@proton.me>
2026-03-12 12:11:00 -0400

    updated function description.

--- dotfiles/linux/home/.config/fish/functions/fuzzy_bat.fish
+++ dotfiles/linux/home/.config/fish/functions/fuzzy_bat.fish
@@ -1,3 +1,3 @@
-function fuzzy_bat -d "Combines fzf with batcat for better previewing of files before selection"
+function fuzzy_bat -d "Combines fzf with bat for better previewing of files before selection"
     fzf --preview 'bat --color=always {}'
 end

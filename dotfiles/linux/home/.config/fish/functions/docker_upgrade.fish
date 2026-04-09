1c85edbe6c3d5415e7960a13994d42fc31a57083
melvinquick <melvinquick@proton.me>
2026-04-09 10:48:01 -0400

    Adding DB indices update command for nextcloud container upgrades.

--- dotfiles/linux/home/.config/fish/functions/docker_upgrade.fish
+++ dotfiles/linux/home/.config/fish/functions/docker_upgrade.fish
@@ -7,6 +7,7 @@ function docker_upgrade -d "While in a directory with a Docker Compose YAML file
     if test $container_name = nextcloud
         sleep 5
         docker compose exec -u www-data app php occ upgrade >/dev/null 2>&1
+        docker compose exec -u www-data app php occ db:add-missing-indices >/dev/null 2>&1
     end
 
     echo 'The container upgrade process is complete!'
